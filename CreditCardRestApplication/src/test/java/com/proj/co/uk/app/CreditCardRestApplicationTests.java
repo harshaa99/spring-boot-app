9f580eace80a639a189e200e7e7f9e79c152d96b
harshaa99 <60323622+harshaa99@users.noreply.github.com>
2020-01-27 22:46:20 +0000

    Updated with Junit tests

--- CreditCardRestApplication/src/test/java/com/proj/co/uk/app/CreditCardRestApplicationTests.java
+++ CreditCardRestApplication/src/test/java/com/proj/co/uk/app/CreditCardRestApplicationTests.java
@@ -1,9 +1,14 @@
 package com.proj.co.uk.app;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.Collections;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,30 +21,45 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import com.proj.co.uk.app.model.CardDetails;
 import com.proj.co.uk.app.service.CreditCardService;
-
+import com.proj.co.uk.app.util.LuhnAlogrithm;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest
 public class CreditCardRestApplicationTests {
-	
+
 	@Autowired
 	MockMvc mockmvc;
-	
+
 	@MockBean
 	CreditCardService service;
 
 	@Test
 	public void contextLoads() throws Exception {
-		
-		when(service.findAll()).thenReturn(
-				Collections.emptyList()
-		);
-		MvcResult mvcresult = mockmvc.perform(
-				MockMvcRequestBuilders.get("/home/showall").accept(MediaType.APPLICATION_CBOR)
-		).andReturn();
-		System.out.println("mvcresultmvcresult"+mvcresult);
+		when(service.findAll()).thenReturn(Collections.emptyList());
+		MvcResult mvcresult = mockmvc
+				.perform(MockMvcRequestBuilders.get("/home/showall").accept(MediaType.APPLICATION_CBOR)).andReturn();
+		System.out.println("mvcresultmvcresult" + mvcresult);
 		verify(service).findAll();
+	}
+
+	@Test
+	public void getAllCardsTest () {
+		when(service.findAll())
+			.thenReturn(Stream.of(new CardDetails("bob", "4444 2222 3333 4444", 200,2000))
+					.collect(Collectors.toList()));
+		assertEquals(1, service.findAll().size());
+	}
+	
+	@Test
+	public void luhnAlgTest () {
+		assertTrue(LuhnAlogrithm.Validate("4689582853001318"));
+	}
+	
+	@Test
+	public void luhnAlgNegativeTest () {
+		assertFalse(LuhnAlogrithm.Validate("46895826653001318"));
 	}
 
 }
