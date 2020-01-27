ec6076f651760833343210e8abf0998ca5bda69e
harshaa99 <60323622+harshaa99@users.noreply.github.com>
2020-01-27 22:47:43 +0000

    updated

--- CreditCardRestApplication/src/main/java/com/proj/co/uk/app/controller/CreditCardController.java
+++ CreditCardRestApplication/src/main/java/com/proj/co/uk/app/controller/CreditCardController.java
@@ -3,12 +3,10 @@ package com.proj.co.uk.app.controller;
 import java.util.Arrays;
 import java.util.List;
 
-import javax.validation.Valid;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +17,7 @@ import com.proj.co.uk.app.model.CardDetails;
 import com.proj.co.uk.app.service.CreditCardService;
 import com.proj.co.uk.app.util.LuhnAlogrithm;
 
+
 @RestController
 @RequestMapping(value = "/home")
 public class CreditCardController {
@@ -28,35 +27,33 @@ public class CreditCardController {
 
 	@GetMapping(value = "/addcard")
 	public ModelAndView home() {
+		logger.info("START of home method->>>>>");
 		ModelAndView mv = new ModelAndView("addcard");
 		mv.addObject("cardDetails", loadCardDetails());
+		logger.info("END of home method->>>>>");
 		return mv;
 	}
 
 	@PostMapping(value = "/showcard")
-	public ModelAndView addCard(@Valid CardDetails cardDetails, BindingResult bResult) {
-		logger.info("inside addCard method->>>>>");
+	public ModelAndView addCard(@Validated CardDetails cardDetails) {
+		logger.info("START of addCard method->>>>>");
 		ModelAndView mv = new ModelAndView();
-		List<CardDetails> details = service.findAll();
-		mv.addObject("cardDetails", details);
-		mv.setViewName("addcard");
-		if (bResult.hasErrors()) {
-			logger.info("Validation errors while submitting form.");
-			return mv;
-		}
 		if (!LuhnAlogrithm.Validate(cardDetails.getCardNumber())) {
 			mv.addObject("carderror", "You have entered wrong card details.");
 			return mv;
-		} else {
-			logger.info("End of addCard method->>>>>>");
-			service.save(cardDetails);
-			details.add(cardDetails);
-			return mv;
-		}
+		} 
+		List<CardDetails> details = service.findAll();
+		mv.addObject("cardDetails", details);
+		mv.setViewName("addcard");
+		service.save(cardDetails);
+		details.add(cardDetails);
+		logger.info("END of addCard method->>>>>>");
+		return mv;
 	}
 
 	@PostMapping(value = "/add")
 	public List<CardDetails> addNewCard(CardDetails cardDetails) {
+		logger.info("START of addNewCard method->>>>>");
 		service.save(cardDetails);
 		return service.findAll();
 	}
@@ -67,10 +64,11 @@ public class CreditCardController {
 	}
 	
 	private List<CardDetails> loadCardDetails() {
-		logger.info("nside loadCardDetails method->>>>>");
-		List<CardDetails> creditCardlist = Arrays.asList(new CardDetails("Alice","6832 7979 9797 9799",-124,2000));
+		logger.info("START of loadCardDetails method->>>>>");
+		List<CardDetails> creditCardlist = Arrays.asList(new CardDetails("Alice", "4244 4434 4343 4324",-124,2000));
 		service.saveAll(creditCardlist);
 		creditCardlist = service.findAll();
+		logger.info("END of loadCardDetails method->>>>>");
 		return creditCardlist;
 	}
 }
